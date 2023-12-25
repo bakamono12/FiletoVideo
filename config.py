aa5775e5111d7760bf26a3cde641230231fb19a2
baka <abhishek>
2023-12-25 21:49:54 +0530

    Added the chnages in Readme also added filter type.

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
-# this file is provided for the copnfigurstion of the Environment variables
+# this file is provided for the configuration of the Environment variables
 # this variable will be used to generate the session string
+# this variable will be used to allow bot to work in the group or chat
 
 
 session_string = os.environ.get("SESSION_STRING", None)
+allowed_users = os.environ.get("ALLOWED_USERS", None)
+allowed_group = os.environ.get("ALLOWED_GROUP", None)
+owner = os.environ.get("OWNER", None)
